a95477febe194d221b877adca056e7202aa38233
lekkss <leksguy97@gmail.com>
2021-12-15 22:47:47 +0100

    initial commit

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/pages/navpages/bar_item_page.dart';
+import 'package:flutter_cubit/pages/navpages/home_page.dart';
+import 'package:flutter_cubit/pages/navpages/profile_page.dart';
+import 'package:flutter_cubit/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    const HomePage(),
+    const SearchPage(),
+    const ProfilePage(),
+    const BarItemPage(),
+  ];
+  int currentIndex = 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(
+              Icons.apps,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Bar",
+            icon: Icon(
+              Icons.bar_chart,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Search",
+            icon: Icon(
+              Icons.search,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Profile",
+            icon: Icon(
+              Icons.person,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
