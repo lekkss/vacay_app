88699e8953a835192de0412354feda23b2a72c4e
lekkss <leksguy97@gmail.com>
2021-12-19 20:35:12 +0100

    button

--- lib/widget/responsive_button.dart
+++ lib/widget/responsive_button.dart
@@ -5,41 +5,45 @@ import 'package:flutter_cubit/widget/app_text.dart';
 class ResponsiveButton extends StatelessWidget {
   bool? isResponsive;
   double? width;
+  final void Function()? onPressed;
   ResponsiveButton({
     Key? key,
     this.isResponsive = false,
     this.width = 120,
-    void Function()? onPressed,
+    this.onPressed,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: AppColors.mainColor,
-        ),
-        width: isResponsive == true ? double.infinity : width,
-        height: 60,
-        child: Row(
-          mainAxisAlignment: isResponsive == true
-              ? MainAxisAlignment.spaceBetween
-              : MainAxisAlignment.center,
-          children: [
-            isResponsive == true
-                ? Container(
-                    margin: const EdgeInsets.only(
-                      left: 20,
-                    ),
-                    child: AppText(
-                      text: "Book Trip Now",
-                      color: Colors.white,
-                    ),
-                  )
-                : Container(),
-            Image.asset("img/button-one.png"),
-          ],
+    return GestureDetector(
+      onTap: onPressed,
+      child: Flexible(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: AppColors.mainColor,
+          ),
+          width: isResponsive == true ? double.infinity : width,
+          height: 60,
+          child: Row(
+            mainAxisAlignment: isResponsive == true
+                ? MainAxisAlignment.spaceBetween
+                : MainAxisAlignment.center,
+            children: [
+              isResponsive == true
+                  ? Container(
+                      margin: const EdgeInsets.only(
+                        left: 20,
+                      ),
+                      child: AppText(
+                        text: "Book Trip Now",
+                        color: Colors.white,
+                      ),
+                    )
+                  : Container(),
+              Image.asset("img/button-one.png"),
+            ],
+          ),
         ),
       ),
     );
